1ba19beabf798fd38ba91e368d4ac23b81ce9a0c
Baha Furkan Yıldızoğlu <baha-furkan@hotmai.com>
2025-12-30 13:21:06 +0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/hospital/automation/HospitalAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospital/automation/HospitalAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
